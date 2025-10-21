89e7840992bf53d36802a2e10dbc80cd39d7bdcd
Amit Pandey <amit@thebranddaddy.com>
2025-10-21 18:46:32 +0400

    Add client/src/components/examples/StatsOverview.tsx

---
+++ client/src/components/examples/StatsOverview.tsx
@@ -0,0 +1,5 @@
+import StatsOverview from '../StatsOverview'
+
+export default function StatsOverviewExample() {
+  return <StatsOverview totalProfit={12450} profitChange={8.5} activePositions={7} positionsChange={2} winRate={68} winRateChange={3} volume24h={45600} volumeChange={-2.3} />
+}
