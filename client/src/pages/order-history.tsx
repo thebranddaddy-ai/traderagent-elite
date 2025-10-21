b2fc2c8096cdecb5c1081b208320048143b0547e
Amit Pandey <amit@thebranddaddy.com>
2025-10-21 18:47:16 +0400

    Add client/src/pages/order-history.tsx

---
+++ client/src/pages/order-history.tsx
@@ -0,0 +1,9 @@
+import EnhancedOrderHistory from "@/components/EnhancedOrderHistory";
+
+export default function OrderHistoryPage() {
+  return (
+    <div className="p-4 md:p-6">
+      <EnhancedOrderHistory />
+    </div>
+  );
+}
