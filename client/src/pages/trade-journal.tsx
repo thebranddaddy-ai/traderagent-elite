d7bea36fcf36141f473f164ccccab4229d5b9346
Amit Pandey <amit@thebranddaddy.com>
2025-10-21 18:47:19 +0400

    Add client/src/pages/trade-journal.tsx

---
+++ client/src/pages/trade-journal.tsx
@@ -0,0 +1,9 @@
+import TradeJournal from "@/components/TradeJournal";
+
+export default function TradeJournalPage() {
+  return (
+    <div className="p-4 md:p-6">
+      <TradeJournal />
+    </div>
+  );
+}
