66053b450110d4b53b6beef510474ecf7577343b
Amit Pandey <amit@thebranddaddy.com>
2025-10-21 18:46:31 +0400

    Add client/src/components/examples/RiskStatusBanner.tsx

---
+++ client/src/components/examples/RiskStatusBanner.tsx
@@ -0,0 +1,5 @@
+import RiskStatusBanner from '../RiskStatusBanner'
+
+export default function RiskStatusBannerExample() {
+  return <RiskStatusBanner isActive={true} riskLevel="medium" message="Market volatility detected - monitoring closely" />
+}
