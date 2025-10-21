893b6c334c039d98b3db3cdc0bd493221d89b3dc
Amit Pandey <amit@thebranddaddy.com>
2025-10-21 18:46:32 +0400

    Add client/src/components/examples/TradingDnaCard.tsx

---
+++ client/src/components/examples/TradingDnaCard.tsx
@@ -0,0 +1,5 @@
+import TradingDnaCard from '../TradingDnaCard'
+
+export default function TradingDnaCardExample() {
+  return <TradingDnaCard winRate={68} avgProfit={145.32} totalTrades={243} riskScore={45} tradingStyle="Aggressive" />
+}
